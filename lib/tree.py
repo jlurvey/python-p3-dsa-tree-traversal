3d767b4b2a34226ec21594ecff104b027849acbf
Joseph Lurvey <joeylurvey@gmail.com>
2023-11-28 20:47:45 -0500

    Complete lab

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,19 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    def depth_first(node):
+        if node is None:
+           return None
+        
+        if node['id'] == id:
+           return node
+        
+        for child in node ['children']:
+           result = depth_first(child)
+           if result: 
+              return result
+        
+        return None
+    
+    return depth_first(self.root)
+    
